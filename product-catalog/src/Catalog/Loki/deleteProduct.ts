81c225e3765678ceed54cdac18fb22c42c9498fb
Sean Donnelly <sdonnelly3000@gmail.com>
2018-12-06 11:25:59 -0500

    :fire: Remove Loki Datastore

--- product-catalog/src/Catalog/Loki/deleteProduct.ts
+++
@@ -1,5 +0,0 @@
-import * as loki from 'lokijs';
-
-export function deleteProduct(db: loki, id: string): Promise<boolean> {
-  throw new Error('Method not implemented.');
-}
--- product-catalog/src/Catalog/Loki/updateProduct.ts
+++
@@ -1,6 +0,0 @@
-import { Product } from '../../shared/Product';
-import * as loki from 'lokijs';
-
-export function updateProduct(db: loki, id: string, name: string, price: number): Promise<Product> {
-  throw new Error('Method not implemented.');
-}
